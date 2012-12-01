b6c8266904a2cb12b038915d4e1b41a08a038c25
Rex Fernando <rex1fernando@gmail.com>
2012-12-01 01:51:43 -0600

    Added delay1 signal to WebSockets module. Delays a signal by 1 second.

--- core-js/Signal/WebSockets.js
+++ core-js/Signal/WebSockets.js
@@ -2,32 +2,49 @@ Elm.WebSockets = function() {
   var JS = Elm.JavaScript;
   var toElmString = Elm.JavaScript.castJSStringToString;
   var toJSString = Elm.JavaScript.castStringToJSString;
-  
-
 
   function sendToWebSocket(socket) {
     return function(input) {
-      socket.send(toJSString(input));
+      if (socket.readyState == WebSocket.OPEN) {
+        console.log(toJSString(input));
+        socket.send(toJSString(input));
+      }
     }
   }
 
-  function webSocket(input) {
-    var socket = new WebSocket("ws://localhost:8080");
-    var socketOpen = false;
-    socket.onopen = function() {
-      socketOpen = true;
+  function webSocket(address, input) {
+    var output = Elm.Signal.constant(["Nil"]);
+    var socket = new WebSocket(toJSString(address));
+    var sender = Elm.Signal.lift(sendToWebSocket(socket))(input);
+    function f(x) { return function(y) { return x; } }
+    var combine = Elm.Signal.lift2(f)(output)(sender);
+    socket.onerror = function(error) {
+      Dispatcher.notify(output.id, toElmString(error));
+      combine = output;
     }
     socket.onmessage = function(event) {
       Dispatcher.notify(output.id, toElmString(event.data));
     }
-    while (!socketOpen) ;
-    var output = Elm.Signal.constant(["Nil"]);
-    var sender = Elm.Signal.lift(sendToWebSocket(socket))(input);
-    function f(x) { return function(y) { return x; } }
-    var combine = Elm.Signal.lift2(f)(output)(sender);
+
     return combine;
   }
 
-  return {webSocket : webSocket
-	  };
+  function delay1handler(output) { 
+    return function(input) {
+      setTimeout(function() { Dispatcher.notify(output.id, input); }, 1000);
+    }
+  }
+
+  function delay1(input) {
+    var output = Elm.Signal.constant(input.value);
+    var delay = Elm.Signal.lift(delay1handler(output))(input);
+    function f(x) { return function(y) { return x; } }
+    var combine = Elm.Signal.lift2(f)(output)(delay);
+    return combine;
+  }
+
+  return {webSocket : function(address) { return function(input) { return webSocket(address, input); }},
+	  delay1 : delay1
+					
+         };
 }();
